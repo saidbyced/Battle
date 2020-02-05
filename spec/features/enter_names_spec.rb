4937e0d41e32dbfc567ca7508c4e35185985b54f
Chris Eadie <chris.eadie@outlook.com>
2020-02-05 17:26:31 +0000

    Added ability/button to attack a player

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: "Foo"
-    fill_in :player_2_name, with: "Bar"
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content "Foo vs. Bar"
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,16 +1,6 @@
 feature 'Viewing hit points' do
   scenario 'see Player 2 hit points' do
-    visit('/')
-    fill_in :player_1_name, with: "Foo"
-    fill_in :player_2_name, with: "Bar"
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content "Bar: 60 HPs"
-  end
-  scenario 'see Player 1 hit points' do
-    visit('/')
-    fill_in :player_1_name, with: "Foo"
-    fill_in :player_2_name, with: "Bar"
-    click_button 'Submit'
-    expect(page).to have_content "Foo: 60 HPs"
   end
 end
