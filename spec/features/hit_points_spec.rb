c7a413480565b88a704cdf068120cc6feef9882f
Chris Eadie <chris.eadie@outlook.com>
2020-02-07 16:37:54 +0000

    Added P1 hit points display

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,4 +1,8 @@
 feature 'Viewing hit points' do
+  scenario 'see Player 1 hit points' do
+    sign_in_and_play
+    expect(page).to have_content "Foo: 60 HPs"
+  end
   scenario 'see Player 2 hit points' do
     sign_in_and_play
     expect(page).to have_content "Bar: 60 HPs"
